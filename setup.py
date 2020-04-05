e7f6d3f3d672635d7b024fd44bc449261fa7e4d4
Andrew Francis <52332881+atfranc2@users.noreply.github.com>
2020-04-05 15:48:33 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,8 @@
-pip install .
+from distutils.core import setup
+
+setup(
+    name='MLclust',
+    version='0.1',
+    packages=['mlclust',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license'
+)
