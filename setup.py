b5a4aaada366275a3db2c79079d8bd700ca30cb4
Andrew Francis <52332881+atfranc2@users.noreply.github.com>
2020-04-05 15:59:57 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from distutils.core import setup
 setup(
     name='MLclust',
     version='0.1',
-    packages=['Cluster', 'VariableSelect', 'Project',  'Standard'],
+    packages=['Clusters', 'VariableSelect', 'Project',  'Standard'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license'
 )
